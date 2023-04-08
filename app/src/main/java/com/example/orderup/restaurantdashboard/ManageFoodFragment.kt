aa85c56f0c6dcee33f88a21aa175350fbb8d90e1
Quocdung <dungttq.21it@vku.udn.vn>
2023-04-08 09:31:40 +0700

    add category and add food

--- app/src/main/java/com/example/orderup/restaurantdashboard/ManageFoodFragment.kt
+++ app/src/main/java/com/example/orderup/restaurantdashboard/ManageFoodFragment.kt
@@ -1,19 +1,25 @@
 package com.example.orderup.restaurantdashboard.ui
 
 import android.annotation.SuppressLint
+import android.app.ProgressDialog
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import android.widget.AdapterView.OnItemClickListener
+import androidx.fragment.app.Fragment
 import com.example.orderup.R
 import com.example.orderup.databinding.FragmentManageFoodBinding
+import com.example.orderup.restaurantdashboard.RestaurantDashboardActivity
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,9 +27,7 @@ private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 private val PICK_IMAGE_REQUEST: Int = 1
 private lateinit var binding: FragmentManageFoodBinding
-
 private lateinit var firebaseAuth: FirebaseAuth
-
 private lateinit var pickImgBtn: ImageButton
 private lateinit var imgIv: ImageView
 private lateinit var foodnameEt: TextInputEditText
@@ -31,7 +35,12 @@ private lateinit var desEt: TextInputEditText
 private lateinit var priceEt: TextInputEditText
 private lateinit var saveBtn: Button
 private lateinit var progressBar: ProgressBar
+private var category:String = ""
+private lateinit var progressDialog: ProgressDialog
+private lateinit var arrayAdapter: ArrayAdapter<*>
+private lateinit var autoText : AutoCompleteTextView
 private lateinit var imgUri: Uri
+var TAG ="ADD_FOOD"
 /**
  * A simple [Fragment] subclass.
  * Use the [RestaurantFragment.newInstance] factory method to
@@ -57,12 +66,29 @@ class ManageFoodFragmentFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding = FragmentManageFoodBinding.inflate(inflater,container,  false)
         firebaseAuth = FirebaseAuth.getInstance()
+        var listCategory = arrayOf(
+            "trang mieng",
+            "sieu ngon",
+            "khai vi",
+        )
+        autoText=binding.categoryAutotext
+        arrayAdapter = ArrayAdapter<String>(container!!.context,R.layout.list_category_addfood, listCategory)
+        autoText.setAdapter(arrayAdapter)
+        autoText.setOnItemClickListener(OnItemClickListener { parent, view, position, id ->
+            category = parent.getItemAtPosition(position).toString()
+
+        })
+
+        progressDialog = ProgressDialog(container!!.context)
+        progressDialog.setTitle("Please wait")
+        progressDialog.setCanceledOnTouchOutside(false)
         pickImgBtn = binding.pickImgBtn
         imgIv = binding.userIv
         foodnameEt = binding.foodnameEt
         desEt = binding.descriptionEt
         priceEt = binding.priceEt
         saveBtn = binding.saveBtn
+        progressBar = binding.progressBar
         pickImgBtn.setOnClickListener {
             openFileChooser()
         }
@@ -80,27 +106,50 @@ class ManageFoodFragmentFragment : Fragment() {
         descripton = desEt.text.toString().trim()
         price = priceEt.text.toString().trim()
         if (foodname.isEmpty()){
-            Toast.makeText(this.context, "Enter your name", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this.context, "Enter your food", Toast.LENGTH_SHORT).show()
         }else if (descripton.isEmpty()){
-            Toast.makeText(this.context, "Enter your Address", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this.context, "Enter your Des", Toast.LENGTH_SHORT).show()
         }else if (price.isEmpty()){
-            Toast.makeText(this.context, "Phone", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this.context, "Price", Toast.LENGTH_SHORT).show()
         }else{
-            uploadInfo()
+           AddCategory()
         }
     }
     @SuppressLint("SuspiciousIndentation")
-    private fun uploadInfo() {
+    private fun AddCategory() {
+        progressDialog.setMessage("Saving....")
         val timestamp = System.currentTimeMillis()
-
         val uid = firebaseAuth.uid!!
-        val ref = FirebaseDatabase.getInstance().getReference("Users/$uid")
-        ref.child("username").setValue(foodname)
-        ref.child("address").setValue(descripton)
-        ref.child("phone").setValue(price)
+        val hashMap: HashMap<String, Any?> =  HashMap()
+        hashMap["id"]="$timestamp"
+        hashMap["uid"] = uid
+        hashMap["foodname"] = foodname
+        hashMap["descripton"] = descripton
+        hashMap["price"] = price
+        hashMap["category"]= category
+        hashMap["timestamp"] = timestamp
+        hashMap["buyCount"]=0
+        hashMap["numberOfLike"]=0
+        Log.i(TAG, "${hashMap}}")
+        Log.i(TAG, "$uid")
+
+        // set data to db
+        val ref = FirebaseDatabase.getInstance().getReference("Foods")
+        ref.child(timestamp.toString())
+            .setValue(hashMap)
             .addOnSuccessListener {
-                Toast.makeText(this.context, "Updated!!!", Toast.LENGTH_SHORT)
+                Log.i(TAG, "luu vao DB success")
+                progressDialog.dismiss()
+//                Toast.makeText(this, "creating account successfully", Toast.LENGTH_SHORT).show()
+//                finish()
+                startActivity(Intent(requireContext(), RestaurantDashboardActivity::class.java))
             }
+            .addOnFailureListener {
+                Log.i(TAG, "luu vao DB that bai")
+                progressDialog.dismiss()
+//                Toast.makeText(this, "Failed creating account due to ${it.message}", Toast.LENGTH_SHORT).show()
+            }
+
     }
     private fun openFileChooser() {
         progressBar.visibility = View.VISIBLE
--- app/src/main/java/com/example/orderup/restaurantdashboard/RestaurantDashboardActivity.kt
+++ app/src/main/java/com/example/orderup/restaurantdashboard/RestaurantDashboardActivity.kt
@@ -1,12 +1,156 @@
 package com.example.orderup.restaurantdashboard
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.ImageButton
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.example.orderup.LoginActivity
 import com.example.orderup.R
+import com.example.orderup.databinding.ActivityRestaurantDashboardBinding
+import com.example.orderup.usedashboard.ViewPageAdapter
+import com.example.orderup.welcome.WelcomeActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 
 class RestaurantDashboardActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRestaurantDashboardBinding
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var toolbar: ImageButton
+    private lateinit var viewPager: ViewPager
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var navigationView: NavigationView
+    private lateinit var header: View
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_restaurant_dashboard)
+        binding = ActivityRestaurantDashboardBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI() {
+        viewPager = binding.viewResPager
+        bottomNavigationView = binding.bottomNav
+
+        val viewPagerAdapter = ViewPagerResAdapter(
+            fragmentManager = supportFragmentManager,
+            FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
+        )
+        viewPager.adapter = viewPagerAdapter
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.tag_home -> {
+                    viewPager.currentItem = 0
+                }
+                R.id.tag_cart -> {
+                    viewPager.currentItem = 1
+                }
+                R.id.tag_favorite -> {
+                    viewPager.currentItem = 2
+                }
+                R.id.tag_order -> {
+                    viewPager.currentItem = 3
+                }
+                R.id.tag_profile -> {
+                    viewPager.currentItem = 4
+                }
+            }
+            true
+        }
+
+        viewPager.addOnPageChangeListener(object: ViewPager.OnPageChangeListener{
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+            }
+            override fun onPageSelected(position: Int) {
+                when (position) {
+                    0 ->{
+                        bottomNavigationView.menu.findItem(R.id.tag_home).isChecked = true
+                        navigationView.menu.findItem(R.id.nav_home).isChecked = true
+                    }
+                    1 ->{
+                            bottomNavigationView.menu.findItem(R.id.tag_cart).isChecked = true
+                            navigationView.menu.findItem(R.id.nav_myCart).isChecked = true
+                    }
+                    2 ->{
+                            bottomNavigationView.menu.findItem(R.id.tag_favorite).isChecked = true
+                            navigationView.menu.findItem(R.id.nav_favorite).isChecked = true
+                    }
+                    4 ->{
+                            bottomNavigationView.menu.findItem(R.id.tag_profile).isChecked = true
+                            navigationView.menu.findItem(R.id.nav_profile).isChecked = true
+                    }
+                    3 ->{
+                            bottomNavigationView.menu.findItem(R.id.tag_order).isChecked = true
+                            navigationView.menu.findItem(R.id.nav_order).isChecked = true
+                    }
+                }
+            }
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+
+        }
+        )
+        navigationView = binding.navView
+        toggle = ActionBarDrawerToggle(
+            this,
+            binding.homeRes,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+
+        binding.homeRes.addDrawerListener(toggle)
+        toggle.syncState()
+        toolbar = binding.toolbar
+        toolbar.setOnClickListener {
+            binding.homeRes.openDrawer(Gravity.LEFT)
+        }
+        navigationView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> {
+                    viewPager.currentItem = 0
+                    true
+                }
+                R.id.nav_myCart -> {
+                        viewPager.currentItem = 1
+                        true
+                }
+                R.id.nav_favorite -> {
+
+                        viewPager.currentItem =2
+                        true
+                }
+                R.id.nav_order -> {
+                        viewPager.currentItem = 3
+                        true
+                }
+                R.id.nav_profile -> {
+                        viewPager.currentItem = 4
+                        true
+                }
+                R.id.nav_logout -> {
+                    firebaseAuth.signOut()
+                    startActivity(Intent(this, WelcomeActivity::class.java))
+                    true
+                }
+                R.id.nav_login ->{
+                    startActivity(Intent(this, LoginActivity::class.java))
+                    true
+                }
+                else -> false
+            }
+        }
+        header = navigationView.getHeaderView(0)
     }
 }
